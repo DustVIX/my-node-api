4fe83af336b2f655c3ea4dbbcdf5c27bb80bc0e0
DustVIX <dustvix@gmail.com>
2025-03-09 06:49:56 +0300

    fix

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/.well-known/discord', (req, res) => {
-    res.send('dh=8ee670f64dcb1be6a82e7786bc7d379cc4559a8f');
-});
+// app.get('/.well-known/discord', (req, res) => {
+//     res.send('dh=8ee670f64dcb1be6a82e7786bc7d379cc4559a8f');
+// });
 
+
+reouter.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 
 app.use("/api/books",bookspath)
