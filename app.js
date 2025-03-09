ba7dad6d76e94288207ab1a38f47e2fa766416da
DustVIX <dustvix@gmail.com>
2025-03-09 06:46:10 +0300

    discord

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/.well-known/discord', (req, res) => {
+    res.send('dh=8ee670f64dcb1be6a82e7786bc7d379cc4559a8f');
+});
+
 
 
 app.use("/api/books",bookspath)
